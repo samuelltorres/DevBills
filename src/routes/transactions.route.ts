283bf39c17cdd6b76bdb00660ae5201102d5f304
samuelltorres <samuelltorres145@gmail.com>
2023-12-13 15:30:02 -0300

    :sparkles: feat: factory + route + validacao da criacao de transacoes

---
+++ src/routes/transactions.route.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import { TransactionsController } from '../controllers/transactions.controller';
+import { createTransactionSchema } from '../dtos/transactions.dto';
+import { TransactionsFactory } from '../factories/transactions.factory';
+import { ParamsType, validator } from '../middleware/validator.middleware';
+
+export const transactionsRoutes = Router();
+
+const controller = new TransactionsController(
+  TransactionsFactory.getServiceInstance(),
+);
+
+transactionsRoutes.post(
+  '/',
+  validator({
+    schema: createTransactionSchema,
+    type: ParamsType.BODY,
+  }),
+  controller.create,
+);
