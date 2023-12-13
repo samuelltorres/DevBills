80bd569c3ee2d3e2b9663daacf63ad3e55782816
samuelltorres <samuelltorres145@gmail.com>
2023-12-13 20:42:51 -0300

    :sparkles: feat: Listagem de transacoes

--- src/database/repositories/transactions.repository.ts
+++ src/database/repositories/transactions.repository.ts
@@ -1,4 +1,7 @@
-import { CreateTransactionDTO } from '../../dtos/transactions.dto';
+import {
+  CreateTransactionDTO,
+  IndexTransactionsDTO,
+} from '../../dtos/transactions.dto';
 import { Transaction } from '../../entities/transactions.entity';
 import { TransactionModel } from '../schemas/transactions.schema';
 
@@ -22,8 +25,25 @@ export class TransactionsRepository {
     return createdTransaction.toObject<Transaction>();
   }
 
-  async index(): Promise<Transaction[]> {
-    const transactions = await this.model.find();
+  async index({
+    title,
+    categoryId,
+    beginDate,
+    endDate,
+  }: IndexTransactionsDTO): Promise<Transaction[]> {
+    const whereParams: Record<string, unknown> = {
+      ...(title && { title: { $regex: title, $options: 'i' } }),
+      ...(categoryId && { 'category._id': categoryId }),
+    };
+
+    if (beginDate || endDate) {
+      whereParams.date = {
+        ...(beginDate && { $gte: beginDate }),
+        ...(endDate && { $lte: endDate }),
+      };
+    }
+
+    const transactions = await this.model.find(whereParams);
     const transactionsMap = transactions.map((item) =>
       item.toObject<Transaction>(),
     );
--- src/dtos/transactions.dto.ts
+++ src/dtos/transactions.dto.ts
@@ -12,3 +12,14 @@ export const createTransactionSchema = {
 
 const createTransactionObject = z.object(createTransactionSchema);
 export type CreateTransactionDTO = z.infer<typeof createTransactionObject>;
+
+// dto responsavel pelos filtros:
+export const indexTransactionsSchema = {
+  title: z.string().optional(),
+  categoryId: z.string().length(24).optional(),
+  beginDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
+};
+
+const indexTransactionsObject = z.object(indexTransactionsSchema);
+export type IndexTransactionsDTO = z.infer<typeof indexTransactionsObject>;
